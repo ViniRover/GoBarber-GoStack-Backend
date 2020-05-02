6a0eb39926fa19ec07895a11a8d8901ff5ba319c
ViniRover <42394125+ViniRover@users.noreply.github.com>
2020-05-02 17:59:56 -0500

    Add files via upload

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,39 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import AppError from '../error/AppError';
+import User from '../models/User';
+
+interface Resquest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Resquest): Promise<User> {
+    const usersRespository = getRepository(User);
+
+    const checkUserExists = await usersRespository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new AppError('Email address already used');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRespository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRespository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
