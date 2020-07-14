31b356a47b07afb7b362981cd7f9121b63d14f96
ViniRover <viniciusrover3@gmail.com>
2020-07-14 19:35:23 -0300

    Email Amazon SES, Upload de imagem Amazon S3, Validação de dados

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'bfac367441e4c647db591290348c1630',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 
@@ -16,9 +17,7 @@ export default class UsersController {
         password,
       });
 
-      delete user.password;
-
-      return res.json(user);
+      return res.json(classToClass(user));
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
